84acaf47f1cb50d96de766f3c9b730cfb054081a
nileshsingh1122 <38974798+nileshsingh1122@users.noreply.github.com>
2018-05-18 11:56:16 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,4 @@
+a=input("enter the value of a:")
+b=input("enter the value of b:")
+c=a-b
+print "the value of c is:", c
---
+++ Generating Series(while loop).py
@@ -0,0 +1,7 @@
+count=0
+while(count<9):
+    print"the count is:",count
+    count=count+1
+    print"good bye"
+
+    
